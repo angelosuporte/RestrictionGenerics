7d0deb685929a4f724bfa9c5cd008fe6a3ac4756
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-05 15:24:06 -0300

    Commit inicial

--- RestrictionGenerics/Program.cs
+++ RestrictionGenerics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using RestrictionGenerics.Services;
 
 namespace RestrictionGenerics
 {
@@ -6,7 +8,25 @@ namespace RestrictionGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> lista = new List<int>();
+
+            Console.Write("Informe a quantidade de Números: ");
+            int quantidadeNumero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadeNumero; i++)
+            {
+                int cadaNumero = int.Parse(Console.ReadLine());
+                lista.Add(cadaNumero);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(lista);
+
+            Console.Write("Max: ");
+            Console.WriteLine(max);
+
+
         }
     }
 }
---
+++ RestrictionGenerics/Services/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestrictionGenerics.Services
+{
+    class CalculationService
+    {
+        public int Max(List<int> lista)
+        {
+            if (lista.Count == 0)
+            {
+                throw new ArgumentException("A lista não pode estar vazia!");
+            }
+
+            int max = lista[0];
+            for (int i = 1; i < lista.Count; i++)
+            {
+                if (lista[i] > max)
+                {
+                    max = lista[i];
+                }
+            }
+            return max;
+        }
+    }
+}
