e0ae37003504e8a6ca1fc3483e905f55fe12d051
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-05 11:05:29 -0300

    Adicionar arquivos de projeto.

---
+++ RestrictionGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestrictionGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
